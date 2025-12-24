58e0d58e151d39ca09431f462b773bc6e99035b0
Afa <afa.alone@gmail.com>
2025-12-24 07:33:41 +0300

    clean

--- lib/moz/bb_codes.ex
+++ lib/moz/bb_codes.ex
@@ -18,85 +18,12 @@ defmodule Moz.BbCodes do
   """
   defp init_context(text) do
     with {:ok, tokens} <- Tokenizer.call(text) do
-      %{ast: [], tokens: tokens, token: nil, type: :text}
+      tokens
     end
   end
 
-  # :process method
-  # token type :open :close :single :text (:smile),
-  # token name,
-  # params,
-  # tokens stream from tokenizer.
-  # top token - text, parser returns its stream and tokens tail
-  # closing token with other than name now converted to text, later can check with tokens stack and autoclose current token
-  # text - add to stream
-  # token - build stream, recursivelly add tokens to stream end
-
-  # stream ended, tokens = []
-  # append token to accum
-  # convert accum to ast item
-  # return ast
-  defp process(%{ast: ast, token: token, tokens: [], type: type}) do
-    ast
+  defp process(tokens) do
+    tokens
     |> Builder.call()
   end
-
-  defp process(%{ast: ast, token: nil, tokens: [head | tail] = tokens, type: _}) do
-    %{type: type} = head
-    IO.inspect(head)
-    IO.inspect(type)
-    process(%{ast: ast, token: head, tokens: tail, type: type})
-  end
-
-  defp process(
-         %{
-           ast: ast,
-           token: token,
-           tokens: [head | tail] = tokens,
-           type: :text
-         } = context
-       ) do
-    process(%{ast: ast, token: head, tokens: tail})
-    # {type, name, params} = ident_token(token)
-    # rez =
-    #   case ident_token(token) do
-    #     {:open, token_name} ->
-    #       make_level(token)
-
-    #     {:close, token_name} ->
-    #       # return(closed(level))
-    #       nil
-
-    #     {:single, list} ->
-    #       # append(to(result and continue))
-    #       nil
-    #   end
-
-    # make_level({})
-  end
-
-  # defp ident_token({:text, list}) do
-  #   {:single}
-  # end
-
-  @doc """
-  ast item: {:type, }
-  type: text, tokenstart, opentokenbody
-
-  return {:ok, level, tail}
-  token_name: current token
-  kind: yieldable/none
-  opts: has or no opts in tag: y/n
-  level: [] of text or tokens with subtree
-  stream: tail of tokens list
-  """
-  # defp make_level({,,,,[]}), do: nil
-  # defp make_level({:token, token_name, {kind, opts}, level, stream}), do: nil
-  # defp make_level({:text, _, _, level, stream}), do: nil
-
-  # ident_token state, prev_token, current_token, token
-  # states: :text, :start_token
-  # defp ident_token(:text, nil, :token_start, )
-
-  # defp ident_token(:text, nil, nil, _), do: :text
 end
